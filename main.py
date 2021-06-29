616fce18497dfc8e21becc53d5060fb7aaed5bc0
GodlevelCoder2275 <76934273+GodlevelCoder2275@users.noreply.github.com>
2021-06-29 11:49:40 +0530

    Add files via upload
    Use PasswordGenerator to secure your device. This helps you to generate strong Password

---
+++ main.py
@@ -0,0 +1,33 @@
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+password_lst = []
+for char in range(1 , nr_numbers+1):
+    random_char = random.choice(numbers)
+    password_lst += random_char
+ 
+
+for char in range(1 , nr_letters+1):
+    password_lst.append(random.choice(letters))
+   
+
+for char in range(1 , nr_symbols+1):
+    random_char = random.choice(symbols)
+    password_lst += random_char
+# print(password_lst)
+
+random.shuffle(password_lst)
+# print(password_lst)
+for i in password_lst:
+    print(i , end="")
+
+
